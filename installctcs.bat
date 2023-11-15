31adc43306a7e0cdb6f35e18ec07cbf681bcb6c6
“Harsha0304” <harsha31reddy@gmail.com>
2023-11-15 22:22:42 +0530

    CTCS Application InSTALL

---
+++ installctcs.bat
@@ -0,0 +1,23 @@
+@echo off
+set "downloadLink=https://testing-script.s3.amazonaws.com/CTCS-Setup_Without_Security.msi"
+set "installerName=CTCS-Setup_Without_Security.msi"
+set "downloadDirectory=C:\Download-Test"
+set "installDirectory=C:\Download-Test\CTCS-Setup_Without_Security.msi"
+
+echo Creating download directory...
+mkdir "%downloadDirectory%" 2>nul
+
+echo Downloading %installerName%...
+curl -o "%downloadDirectory%\%installerName%" "%downloadLink%" || (
+    echo Failed to download %installerName%. Exiting.
+    exit /b 1
+)
+
+echo Installing %installerName%...
+start /wait msiexec /i "%downloadDirectory%\%installerName%" /qn INSTALLDIR="%installDirectory%" || (
+    echo Failed to install %installerName%. Exiting.
+    exit /b 1
+)
+
+echo Installation completed successfully. Restarting the system...
+shutdown /r /t 0
