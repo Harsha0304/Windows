a05dbb0c0845e1b2138cfdcd9a2f87c7b79721a6
Harsha0304 <harsha31reddy@gmail.com>
2024-02-02 11:11:14 +0530

    Sysinfo-Ver2

--- Sysinfo.bat
+++ Sysinfo.bat
@@ -32,6 +32,16 @@ for /f "tokens=2 delims=:" %%a in ('systeminfo ^| findstr /C:"Total Physical Mem
 for /f "delims=" %%a in ('powershell -command "Get-NetAdapter | Where-Object { $_.Name -like '*Wi-Fi*' } | Select-Object -ExpandProperty MacAddress"') do set "macAddress=%%a" && goto :next7
 :next7
 
+:: Get disk storage information using PowerShell
+powershell -command "Get-CimInstance Win32_DiskDrive | Select-Object Caption,Model,Size | ForEach-Object { 'Drive: ' + $_.Caption + ', Model: ' + $_.Model + ', Size: ' + [math]::Round($_.Size / 1GB, 2) + ' GB' } | Out-File temp_disk_info.txt -Encoding utf8"
+
+:: Read the temporary file and set the variable
+set /p diskInfo=<temp_disk_info.txt
+
+:: Delete the temporary file
+del temp_disk_info.txt
+
+
 :: Write information to the output file
 echo Device Manufacturer: %manufacturer% > "%outputFile%"
 echo System Model: %systemModel% >> "%outputFile%"
@@ -40,6 +50,7 @@ echo System Type: %systemType% >> "%outputFile%"
 echo Processor: %processor% >> "%outputFile%"
 echo Total Physical Memory: %totalMemory% >> "%outputFile%"
 echo MAC Address: %macAddress% >> "%outputFile%"
+echo Disk Storage Information: %diskInfo% >> "%outputFile%"
 
 :: Display success message
 echo Information has been saved to %outputFile%
